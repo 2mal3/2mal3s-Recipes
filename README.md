dd5c722405c4bcc1dbaaaf5565f59228b59501af
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-07-28 23:45:50 +0200

    feat: add actual datapack files

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# datapack-template
+# 2mal3s-Recipes
+
+![GitHub release (latest by date)](https://img.shields.io/github/v/release/2mal3/2mal3s-Recipes?style=flat-square) ![GitHub all releases](https://img.shields.io/github/downloads/2mal3/2mal3s-Recipes/total?style=flat-square) ![Minecraft](https://img.shields.io/badge/Minecraft-1.20.4-orange?style=flat-square)
+
+The 2mal3's Recipes datapack adds, changes, fixes and upgrades over 400 different, well considered recipes to improve the overall game experience while still keeping the vanilla feel. Unlike other datapacks, it doesn't add overpowered or unbalanced recipes for like Totems or Elytras that ruin the balance of the game, but only makes subtle changes that won't disrupt on the gameplay. In addition, all recipes are deeply integrated into the recipe book with groups and advancements, so that you do not have to remember any recipes and automatically learn about them when you have the necessary resources.
+
+## 📖 Features
+
+- over 400 recipes
+- recipes are automatically unlocked as you obtain the ingredients.
+- each recipe is integrated into the recipe book with groups and categories
+- no overpowered recipes
+
+## 📝 Some Recipes
+
+- recipes for previously uncraftable items such as saddles, name tags, horse armours, leads, bundles, chain armour, corals, bells, ores
+- recipes for recycling iron, gold and diamond gear and uncrafting ice, maps, quartz, mushrooms, stairs, slabs and more
+- cheaper stairs, buttons, pressure plates, trapdoors, hoppers, ice, prismarine, TNT, chains and netherite blocks
+- more expensive shields, bonemeal, hay ball
+- wood can be cut in the stonecutter
+- recipes for quicker crafting for example sticks, chests, minecarts
+- coloured concrete can be recoloured
+- dye can be mixed more and made from more resources
+- some small recipes are shapeless
+
+## Technical Notes
+
+- **ONLY** the latest version of the datapack for Fabric and Vanilla 1.20.4 is supported
+- bug reports and suggestions are **ONLY** accepted on [GitHub](https://github.com/2mal3/2mal3s-Recipes/issues)
+- compatible with other datapacks that do not add or change too many recipes themselves
+- to uninstall, simply delete the datapack file
+- build using [beet](https://github.com/mcbeet/beet)
---
+++ plugins/generate_recipe_advancements.py
@@ -0,0 +1,111 @@
+from beet import Context, Advancement
+
+
+def beet_default(ctx: Context):
+    advancements = get_advancements(ctx)
+    create_advancements(ctx, advancements)
+
+
+def create_advancements(ctx: Context, data: dict):
+    for ingredient, paths in data.items():
+        name = ingredient.split(":")[1]
+        advancement = {
+            "parent": "minecraft:recipes/root",
+            "criteria": {
+                "requirement": {
+                    "trigger": "minecraft:inventory_changed",
+                    "conditions": {"items": [{}]},
+                }
+            },
+            "rewards": {"recipes": paths},
+        }
+
+        # Normal advancement
+        if not ingredient.startswith("#"):
+            advancement["criteria"]["requirement"]["conditions"]["items"][0][
+                "items"
+            ] = [ingredient]
+        # Tag
+        else:
+            advancement["criteria"]["requirement"]["conditions"]["items"][0][
+                "tag"
+            ] = ingredient[1:]
+
+        ctx.data.advancements[f"2re:recipes/{name}"] = Advancement(advancement)
+
+
+def get_advancements(ctx: Context) -> dict:
+    """Get all new recipe results with their ingredients"""
+    advancements = {}
+
+    recipes = ctx.data.recipes
+    for recipe in recipes.match("2re:*"):
+        content = recipes[recipe].data
+
+        ingredients = []
+        if content["type"] == "minecraft:crafting_shaped":
+            ingredients = get_crafting_shaped_ingredients(content["key"])
+        if content["type"] == "minecraft:crafting_shapeless":
+            ingredients = get_ingredient(content["ingredients"])
+        if content["type"] in [
+            "minecraft:blasting",
+            "minecraft:smelting",
+            "minecraft:smoking",
+            "minecraft:campfire_cooking",
+            "minecraft:stonecutting",
+        ]:
+            ingredients = get_ingredient(content["ingredient"])
+        ingredients = remove_duplicates(ingredients)
+
+        ingredients.append(get_result(content["result"]))
+
+        for ingredient in ingredients:
+            if ingredient not in advancements:
+                advancements[ingredient] = []
+            advancements[ingredient].append(recipe)
+
+    return advancements
+
+
+def get_result(part: dict | list) -> str:
+    result = ""
+
+    if type(part) == dict:
+        result = part["item"]
+    if type(part) == str:
+        result = part
+
+    return result
+
+
+def get_ingredient(part: dict | list) -> list:
+    ingredients = []
+
+    if type(part) == dict:
+        ingredient = part.get("item")
+        if ingredient:
+            ingredients.append(ingredient)
+
+        ingredient = part.get("tag")
+        if ingredient:
+            ingredients.append("#" + ingredient)
+
+    if type(part) == list:
+        for element in part:
+            ingredient = get_ingredient(element)
+            ingredients += ingredient
+
+    return ingredients
+
+
+def get_crafting_shaped_ingredients(keys: dict) -> list:
+    ingredients = []
+
+    for element in keys.values():
+        ingredients += get_ingredient(element)
+
+    return ingredients
+
+
+def remove_duplicates(list_: list) -> list:
+    return list(set(list_))
