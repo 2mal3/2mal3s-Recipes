04f8629e29f9c0f29c1d824a44c0bb0c5cf946fa
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-01-15 23:20:06 +0100

    ref: removed unused code

--- plugins/remove_empty_functions.py
+++
@@ -1,8 +0,0 @@
-from beet import Context
-
-
-def beet_default(ctx: Context):
-    funcs = ctx.data.functions
-    for path in funcs.match("*"):
-        if not len(funcs[path].lines):
-            funcs.pop(path)
